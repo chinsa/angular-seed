2ce4d6b12ef7605b3173d4bd59ff6251eeb5d0f7
Pete Bacon Darwin <pete@bacondarwin.com>
2012-02-23 22:16:55 +0000

    Refactored to a more page oriented approach

--- app/coffee-script/controllers.coffee
+++ app/coffee-script/controllers.coffee
@@ -1,17 +1,38 @@
 'use strict'
+class Page
+  constructor: (@index, @title, @description, @type='simple')->
+
+class QuestionPage extends Page
+  constructor: (@question, index)->
+    super(index, @question.title, @question.description, "question-#{@question.type}")
+
+class IntroPage extends Page
+
+class ResponsePage extends Page
+  constructor: (@questionPages, index, title, description)->
+    super(title, description, 'response')
 
 # App Controllers
 class @SurveyController
   @$inject: ['Survey']
   constructor: (Survey)->
-    @survey = Survey.get()
+    @survey = Survey.get( ()=>
+      @pages = []
+      @pages.push(new IntroPage(0, @survey.title, @survey.description))
+      questionPages = []
+      for question, index in @survey.questions
+        page = new QuestionPage(question, index+1)
+        questionPages.push(page)
+      Array::push.apply(@pages,questionPages)
+      @pages.push(new ResponsePage(questionPages, @pages.length))
+    )
     @currentPage = 0
 
   isCurrentPage: (page)->
-    page is @survey.pages[@currentPage]
+    page.index is @currentPage
 
   hasNextPage: ()->
-    @currentPage < @survey.pages.length-1
+    @currentPage < @pages.length-1
 
   hasPreviousPage: ()->
     @currentPage > 0
@@ -22,11 +43,11 @@ class @SurveyController
   goToPreviousPage: ()->
     @currentPage -= 1
 
-  goToPage: (page)->
-    @currentPage = i for p, i in @survey.pages when p is page
+  goToPage: (pageIndex)->
+    @currentPage = pageIndex
 
-  isValid: (page)->
-    page.response?
+  canMoveToNext: (page)->
+    page.response? or not page.question?
 
   selectChoice: (page, choice)->
     page.response = choice
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,20 +1,89 @@
 (function() {
   'use strict';
+  var IntroPage, Page, QuestionPage, ResponsePage,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  Page = (function() {
+
+    function Page(index, title, description, type) {
+      this.index = index;
+      this.title = title;
+      this.description = description;
+      this.type = type != null ? type : 'simple';
+    }
+
+    return Page;
+
+  })();
+
+  QuestionPage = (function(_super) {
+
+    __extends(QuestionPage, _super);
+
+    function QuestionPage(question, index) {
+      this.question = question;
+      QuestionPage.__super__.constructor.call(this, index, this.question.title, this.question.description, "question-" + this.question.type);
+    }
+
+    return QuestionPage;
+
+  })(Page);
+
+  IntroPage = (function(_super) {
+
+    __extends(IntroPage, _super);
+
+    function IntroPage() {
+      IntroPage.__super__.constructor.apply(this, arguments);
+    }
+
+    return IntroPage;
+
+  })(Page);
+
+  ResponsePage = (function(_super) {
+
+    __extends(ResponsePage, _super);
+
+    function ResponsePage(questionPages, index, title, description) {
+      this.questionPages = questionPages;
+      ResponsePage.__super__.constructor.call(this, title, description, 'response');
+    }
+
+    return ResponsePage;
+
+  })(Page);
+
   this.SurveyController = (function() {
 
     SurveyController.$inject = ['Survey'];
 
     function SurveyController(Survey) {
-      this.survey = Survey.get();
+      var _this = this;
+      this.survey = Survey.get(function() {
+        var index, page, question, questionPages, _len, _ref;
+        _this.pages = [];
+        _this.pages.push(new IntroPage(0, _this.survey.title, _this.survey.description));
+        questionPages = [];
+        _ref = _this.survey.questions;
+        for (index = 0, _len = _ref.length; index < _len; index++) {
+          question = _ref[index];
+          page = new QuestionPage(question, index + 1);
+          questionPages.push(page);
+        }
+        Array.prototype.push.apply(_this.pages, questionPages);
+        return _this.pages.push(new ResponsePage(questionPages, _this.pages.length));
+      });
       this.currentPage = 0;
     }
 
     SurveyController.prototype.isCurrentPage = function(page) {
-      return page === this.survey.pages[this.currentPage];
+      return page.index === this.currentPage;
     };
 
     SurveyController.prototype.hasNextPage = function() {
-      return this.currentPage < this.survey.pages.length - 1;
+      return this.currentPage < this.pages.length - 1;
     };
 
     SurveyController.prototype.hasPreviousPage = function() {
@@ -29,19 +98,12 @@
       return this.currentPage -= 1;
     };
 
-    SurveyController.prototype.goToPage = function(page) {
-      var i, p, _len, _ref, _results;
-      _ref = this.survey.pages;
-      _results = [];
-      for (i = 0, _len = _ref.length; i < _len; i++) {
-        p = _ref[i];
-        if (p === page) _results.push(this.currentPage = i);
-      }
-      return _results;
+    SurveyController.prototype.goToPage = function(pageIndex) {
+      return this.currentPage = pageIndex;
     };
 
-    SurveyController.prototype.isValid = function(page) {
-      return page.response != null;
+    SurveyController.prototype.canMoveToNext = function(page) {
+      return (page.response != null) || !(page.question != null);
     };
 
     SurveyController.prototype.selectChoice = function(page, choice) {
