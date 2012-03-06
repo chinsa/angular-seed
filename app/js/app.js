0717c6dd040bf874cba332403c461f5fcb8b7ff5
Pete Bacon Darwin <pete@bacondarwin.com>
2012-03-06 22:33:29 +0000

    Removed RouteHandler calls

--- app/js/app.js
+++ app/js/app.js
@@ -3,33 +3,7 @@
   /* http://docs.angularjs.org/#!angular.service
   */
   namespace("Questionnaire", {
-    QuestionnaireApplication: [
-      'RouteHandler', '$rootScope', '$routeParams', function(RouteHandler, $rootScope, $routeParams) {
-        var _this = this;
-        RouteHandler.handle('/', {
-          template: '/templates/questionnaire-list.html'
-        });
-        RouteHandler.handle('/:questionnaire', {
-          template: '/templates/questionnaire-details.html',
-          handler: function(current) {
-            return $rootScope.questionnaireId = $routeParams.questionnaire;
-          }
-        });
-        RouteHandler.handle('/:questionnaire/summary', {
-          template: '/templates/questionnaire-summary.html',
-          handler: function(current) {
-            return $rootScope.questionnaireId = $routeParams.questionnaire;
-          }
-        });
-        return RouteHandler.handle('/:questionnaire/:question', {
-          template: '/templates/question.html',
-          handler: function(current) {
-            $rootScope.questionnaireId = $routeParams.questionnaire;
-            return $rootScope.questionIndex = Number($routeParams.question);
-          }
-        });
-      }
-    ]
+    QuestionnaireApplication: function() {}
   });
 
 }).call(this);
--- app/src/app.coffee
+++ app/src/app.coffee
@@ -3,25 +3,5 @@
 
 namespace "Questionnaire"
   QuestionnaireApplication:
-    ['RouteHandler', '$rootScope', '$routeParams', (RouteHandler, $rootScope, $routeParams)->
+    ()->
     
-      RouteHandler.handle('/',
-        template: '/templates/questionnaire-list.html'
-      )
-      RouteHandler.handle('/:questionnaire',
-        template: '/templates/questionnaire-details.html'
-        handler: (current)=>
-          $rootScope.questionnaireId = $routeParams.questionnaire
-      )
-      RouteHandler.handle('/:questionnaire/summary',
-        template: '/templates/questionnaire-summary.html',
-        handler: (current)=>
-          $rootScope.questionnaireId = $routeParams.questionnaire
-      )
-      RouteHandler.handle('/:questionnaire/:question',
-        template: '/templates/question.html'
-        handler: (current)=>
-          $rootScope.questionnaireId = $routeParams.questionnaire
-          $rootScope.questionIndex = Number($routeParams.question)
-      )
-    ]
